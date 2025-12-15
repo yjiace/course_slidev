f6f6364d86b03d5eebb35013c5164392599d9cff
smallyoung <504015995@qq.com>
2025-12-15 13:28:08 +0800

    test

--- portal/.vitepress/theme/components/MindMapFloat.vue
+++ portal/.vitepress/theme/components/MindMapFloat.vue
@@ -21,11 +21,11 @@ const calculateButtonPosition = () => {
   if (contentEl) {
     const rect = contentEl.getBoundingClientRect()
     // 按钮应该在内容区域右边缘外侧，更靠右显示
-    const rightOffset = window.innerWidth - rect.right - 32
-    buttonRight.value = `${Math.max(16, rightOffset)}px`
+    const rightOffset = window.innerWidth - rect.right - 128
+    buttonRight.value = `${Math.max(8, rightOffset)}px`
   } else {
     // 如果找不到内容区域，使用默认值
-    buttonRight.value = '16px'
+    buttonRight.value = '8px'
   }
 }
 
